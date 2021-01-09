5dbc2860491d823ec781e9eab04218cf9b9a2ea4
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-09 19:43:31 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -2,9 +2,15 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {}
+      :matriarch => {}
+      :hero => {}
+      :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch => {}
+      :matriarch => {}
+      :heroine => {}
+      :heroine_friends => []
       }}
 
   
